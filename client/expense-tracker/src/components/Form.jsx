68cf1d9c154eb551c3403f162f45787e50729b27
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-12-22 00:58:53 +0100

    reactHookForm

--- client/expense-tracker/src/components/Form.jsx
+++ client/expense-tracker/src/components/Form.jsx
@@ -1,28 +1,36 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
+import List from './List';
+
 
 const Form = () => {
+
+  const {register,handleSubmit,resetField} = useForm();
+  const onSubmit = data => console.log(data);
   return (
     <div className='form max-w-sm mx-auto w-96'>
       <h1 className='font-bold pb-4 text-xl'>Transaction</h1>
-      <form id="form">
+      <form id="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-4">
           <div className="input-group">
-            <input type="text" placeholder="Salary, House Rent, SIP" className="form-input px-1" />
+            <input type="text" {...register("name")} placeholder="Salary, House Rent, SIP" className="form-input px-1" />
           </div>
-          <select name="" id="">
+          <select className='form-input' {...register("type")} >
             <option value="investment" defaultValue>Investment</option>
-            \
             <option value="investment" >Expenses</option>
             <option value="investment" >Savings</option>
           </select>
           <div className="input-group">
-            <input type="text" placeholder="Amount" className="form-input px-1" />
+            <input type="text" placeholder="Amount"{...register("amount")}className="form-input px-1" />
           </div>
           <div className="submit-btn">
             <button type="submit" className='border py-2 text-white bg-indigo-500 w-full'>Make Transaction</button>
           </div>
         </div>
       </form>
+
+      <List />
+      
     </div>
   )
 }
---
+++ client/expense-tracker/src/components/List.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const List = () => {
+  return (
+    <div className='flex flex-col py-6 gap-3'>
+      <h1 className='py-4 font-bold text-xl'>History</h1>
+      <div className="item flex justify-center bg-gray-50 py-2 rounded-r">
+        <span className='block w-full'></span>
+      </div>
+    </div>
+  )
+}
+
+export default List
