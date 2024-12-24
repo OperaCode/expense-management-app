84c04667506030223f91320617d413267f648524
raphael faboyinde <raphaelfaboyinde27@gmail.com>
2024-12-24 10:12:25 +0100

    updatefolders

--- client/expense-tracker/src/components/List.jsx
+++ client/expense-tracker/src/components/List.jsx
@@ -19,14 +19,14 @@ const categories = [
 
 const List = () => {
   return (
-    <div className="flex flex-col py-6 gap-3">
+    <div className="flex flex-col py-5 gap-3">
       <h1 className="py-4 font-bold text-xl">History</h1>
 
       {categories.map((category, index) => (
         <div
         key={index}
-        className="item flex justify-center bg-gray-50 py-2 rounded-r"
-        style={{ borderRight: `8px solid${category.color ?? "#e5ee5"}` }}
+        className="item flex justify-center bg-gray-50 py-1 rounded-r"
+        style={{ borderRight:`8px solid${category.color ?? "#e5ee5"}` }}
       >
         <button className="px-3"><box-icon name="trash" size="15px" color= "#a13d38"></box-icon></button>
         <span className="block w-full">{category.name?? "Savings"}</span>
