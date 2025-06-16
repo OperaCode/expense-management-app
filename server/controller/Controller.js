0392c16914f92435de3fade6c3b8b4b4a8fb6cc5
Raphael Faboyinde <raphaelfaboyinde27@gmail.com>
2025-06-16 23:31:47 +0100

    debug

--- server/controller/Controller.js
+++ server/controller/Controller.js
@@ -1,122 +1,97 @@
 const model = require("../models/model");
 
-// get categories
-
-// post:http://localhost:8000/api/categories
-const createCategories = (req, res) => {
+//  post: http://localhost:8080/api/categories
+async function createCategories(req, res){
   const Create = new model.Categories({
-    type: "Expense",
-    color: "#1F3B5C", //dark
-  });
-  Create.save(function (err) {
-    if (err) return res.json(Create);
-    return res
-      .status(400)
-      .json({ message: `Error whie creating categories ${err}` });
-  });
-};
-// get:http://localhost:8000/api/categories
-const getCategories = async (req, res) => {
-  try {
-    const data = await model.Categories.find({});
-
-    // Extract only `type` and `color`
-    const filter = data.map((v) => ({
-      type: v.type,
-      color: v.color,
-    }));
-
-    return res.status(200).json(filter); // return filtered data, not full data
-  } catch (error) {
-    console.error("Error fetching categories:", error);
-    return res.status(500).json({ error: error.message });
-  }
-};
-
-
-// post:http://localhost:8000/api/transaction
-const createTransaction = async (req, res) => {
-  try {
-    const { name, type, amount } = req.body;
-
-    if (!name || !type || !amount) {
-      return res.status(400).json({ error: "Missing required fields" });
-    }
-
-    const create = await new model.Transaction({
-      name,
-      type,
-      amount,
-      date: new Date(),
-    });
-
-    await create.save();
-
-    console.log("Transaction created");
-    return res.status(201).json({ message: "Transaction created", data: create });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-// get:http://localhost:8000/api/transaction
-const getTransaction = async (req, res) => {
-  let data = await model.Transaction.find({});
-  return res.json(data);
-};
-
-// get:http://localhost:8000/api/transaction
-const deleteTransaction = async (req, res) => {
-  if (!req.body) res.status(400).json({ message: "Request body not Found" });
-  await model.Transaction.deleteOne(req.body, function (err) {
-    if (!err) res.json("Record Deleted...!");
+      type: "Investment",
+      color: "#FCBE44"
   })
-    .clone()
-    .catch(function (err) {
-      res.json({ message: "Error while deleting transaction Record" });
-    });
-};
 
-// get:http://localhost:8000/api/transaction
-const getLabels = async (req, res) => {
-  model.Transaction.aggregate([
-    {
-      $lookup: {
-        from: "categories",
-        localField: "type",
-        foreignField: "type",
-        as: "CategoriesInfo",
-      },
-    },
-    {
-      $unwind: "$categoriesInfo",
-    },
-  ])
-    .then((result) => {
-      const data = result.map((v) =>
-        Object.assign(
-          {},
-          {
-            _id: v._id,
-            name: v.name,
-            type: v.type,
-            amount: v.amount,
-            color: v.categoriesInfo["color"],
-          }
-        )
-      );
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(400).json("Lookup Collections Error");
-    });
-};
+  await Create.save(function(err){
+      if (!err) return res.json(Create);
+      return res.status(400).json({ message : `Error while creating categories ${err}`});
+  });
+}
+
+//  get: http://localhost:8080/api/categories
+async function  getCategories(req, res){
+   let data = await model.Categories.find({})
+
+   let filter = await data.map(v => Object.assign({}, { type: v.type, color: v.color}));
+   return res.json(filter);
+}
+
+//  post: http://localhost:8080/api/transaction
+const createTransaction = async(req, res)=>{
+   try {
+    // if(!req.body) return res.status(400).json("Post HTTP Data not Provided");
+   const  { name, type, amount } = req.body;
+
+   if (!name || !type || !amount){
+    return res.status(500).json({message: "All fields are required"})
+   }
+
+   const create = await new model.Transaction(
+       {
+           name,
+           type,
+           amount,
+           date: new Date()
+       }
+   );
+
+   create.save();
+   return res.json(create);
+   } catch (error) {
+    console.log(err)
+    return res.status(400).json({ message : `Error while creating transaction ${err}`});
+   }
+
+}
+
+//  get: http://localhost:8080/api/transaction
+async function getTransaction(req, res){
+   let data = await model.Transaction.find({});
+   return res.json(data);
+}
+
+//  delete: http://localhost:8080/api/transaction
+async function deleteTransaction(req, res){
+   if (!req.body) res.status(400).json({ message: "Request body not Found"});
+   await model.Transaction.deleteOne(req.body, function(err){
+       if(!err) res.json("Record Deleted...!");
+   }).clone().catch(function(err){ res.json("Error while deleting Transaction Record")});
+}
+
+//  get: http://localhost:8080/api/labels
+async function getLabels(req, res){
+
+   model.Transaction.aggregate([
+       {
+           $lookup : {
+               from: "categories",
+               localField: 'type',
+               foreignField: "type",
+               as: "categories_info"
+           }
+       },
+       {
+           $unwind: "$categories_info"
+       }
+   ]).then(result => {
+       let data = result.map(v => Object.assign({}, { _id: v._id, name: v.name, type: v.type, amount: v.amount, color: v.categories_info['color']}));
+       res.json(data);
+   }).catch(error => {
+       res.status(400).json("Looup Collection Error");
+   })
+
+}
 
 module.exports = {
-  createCategories,
-  getCategories,
-  createTransaction,
-  getTransaction,
-  deleteTransaction,
-  getLabels,
-};
+   createCategories,
+   getCategories,
+   createTransaction,
+   getTransaction,
+   deleteTransaction,
+   getLabels
+}
